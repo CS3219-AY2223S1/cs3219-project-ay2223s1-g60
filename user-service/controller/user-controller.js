29bc7f74c144ecafc102e1ea92545e8138a67fdc
Florencia Martina <florenciamartina15@gmail.com>
2022-09-02 13:18:21 +0800

    Store salted and hashed password to user DB

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,21 +1,34 @@
-import { ormCreateUser as _createUser } from '../model/user-orm.js'
+import { ormCreateUser as _createUser } from "../model/user-orm.js";
+import bcrypt from "bcrypt";
 
 export async function createUser(req, res) {
-    try {
-        const { username, password } = req.body;
-        if (username && password) {
-            const resp = await _createUser(username, password);
-            console.log(resp);
-            if (resp.err) {
-                return res.status(400).json({message: 'Could not create a new user!'});
-            } else {
-                console.log(`Created new user ${username} successfully!`)
-                return res.status(201).json({message: `Created new user ${username} successfully!`});
-            }
-        } else {
-            return res.status(400).json({message: 'Username and/or Password are missing!'});
-        }
-    } catch (err) {
-        return res.status(500).json({message: 'Database failure when creating new user!'})
+  try {
+    const { username, password } = req.body;
+    let saltRounds = parseInt(process.env.SALT_ROUNDS);
+
+    if (username && password) {
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+      const resp = await _createUser(username, hashedPassword);
+      console.log(resp);
+      if (resp.err) {
+        return res
+          .status(400)
+          .json({ message: "Could not create a new user!" });
+      } else {
+        console.log(`Created new user ${username} successfully!`);
+        return res
+          .status(201)
+          .json({ message: `Created new user ${username} successfully!` });
+      }
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Username and/or Password are missing!" });
     }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Database failure when creating new user!" });
+  }
 }
