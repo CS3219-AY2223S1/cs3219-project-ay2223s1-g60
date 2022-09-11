ac55388b40f13551f0e5157edc1ae8986f9a10bb
Erin May Gunawan <erinmaygunawan@gmail.com>
2022-09-11 20:58:31 +0800

    Use MUI

--- frontend/src/components/MatchingPage.tsx
+++ frontend/src/components/MatchingPage.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import '../matching-page.css';
+import React, { useState } from 'react';
+import { Box, Button, Stack, Typography } from '@mui/material';
 
 function MatchingPage() {
+  const [loading, setLoading] = useState(false);
+  const [difficulty, setDifficulty] = useState(0);
+
   return (
     <div className='matching-page'>
-      <div className='box'>
-        <h1>Difficulty Setting</h1>
-        <button>Easy</button>
-        <button>Medium</button>
-        <button>Hard</button>
-      </div>
+      <Box display={'flex'} flexDirection={'column'} width={'30%'}>
+        <Typography variant={'h3'} marginBottom={'2rem'}>
+          Difficulty Setting
+        </Typography>
+        <Stack spacing={2}>
+          <Button variant={'outlined'} onClick={() => setDifficulty(1)}>
+            Easy
+          </Button>
+          <Button variant={'outlined'} onClick={() => setDifficulty(2)}>
+            Medium
+          </Button>
+          <Button variant={'outlined'} onClick={() => setDifficulty(3)}>
+            Hard
+          </Button>
+          <Button variant={'contained'} onClick={() => setLoading(true)}>
+            Find Match
+          </Button>
+        </Stack>
+      </Box>
     </div>
   );
 }
