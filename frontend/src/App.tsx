69a1fecf19a55f0cda6806c801840897a288190b
Erin May Gunawan <erinmaygunawan@gmail.com>
2022-09-14 21:42:42 +0800

    Add theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,25 +4,31 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+import { theme } from './styles';
 import SignupPage from './components/SignupPage';
 import MatchingPage from './components/MatchingPage';
 import RoomPage from './views/RoomPage';
-import { Box } from '@mui/material';
+import { Box, ThemeProvider } from '@mui/material';
 import React from 'react';
 
 function App() {
   return (
     <div className='App'>
-      <Box display={'flex'} flexDirection={'column'} padding={'4rem'}>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Navigate replace to='/signup' />}></Route>
-            <Route path='/signup' element={<SignupPage />} />
-            <Route path='/match' element={<MatchingPage />} />
-            <Route path='/room' element={<RoomPage />} />
-          </Routes>
-        </Router>
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Box display={'flex'} flexDirection={'column'} padding={'4rem'}>
+          <Router>
+            <Routes>
+              <Route
+                path='/'
+                element={<Navigate replace to='/signup' />}
+              ></Route>
+              <Route path='/signup' element={<SignupPage />} />
+              <Route path='/match' element={<MatchingPage />} />
+              <Route path='/room' element={<RoomPage />} />
+            </Routes>
+          </Router>
+        </Box>
+      </ThemeProvider>
     </div>
   );
 }
