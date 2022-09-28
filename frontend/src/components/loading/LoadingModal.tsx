c2cc740db32ec1ae376664b04a9bf79ea75b6b24
jzwoo <jzwoo98@gmail.com>
2022-09-29 01:27:31 +0800

    Add redirect after match

--- frontend/src/components/loading/LoadingModal.tsx
+++ frontend/src/components/loading/LoadingModal.tsx
@@ -1,70 +1,81 @@
 import React from "react";
-import {Dialog, Stack, Box, Button, CircularProgress, DialogTitle} from "@mui/material";
-import {io} from "socket.io-client";
+import {
+  Dialog,
+  Stack,
+  Box,
+  Button,
+  CircularProgress,
+  DialogTitle,
+} from "@mui/material";
+import { io } from "socket.io-client";
+import { useNavigate } from "react-router-dom";
 
 type LoadingModalProps = {
-    open: boolean;
-    closeModal: () => void;
-    userId: number;
-    difficulty: number;
-}
+  open: boolean;
+  closeModal: () => void;
+  userId: number;
+  difficulty: number;
+};
 
 const LoadingModal: React.FC<LoadingModalProps> = (props) => {
-    const {open, closeModal, difficulty, userId} = props;
-    const socket = io("http://localhost:8001");
+  const { open, closeModal, difficulty, userId } = props;
+  const socket = io("http://localhost:8001");
+  const navigate = useNavigate();
 
-    // timeout
-    const startCountDown = () => setTimeout(() => {
-        socket.disconnect();
-        closeModal();
+  // timeout
+  const startCountDown = () =>
+    setTimeout(() => {
+      socket.disconnect();
+      closeModal();
     }, 30000);
 
-    socket.on("connect", () => {
-        console.log(`${socket.id} is trying to connect`);
+  socket.on("connect", () => {
+    console.log(`${socket.id} is trying to connect`);
 
-        socket.emit("find-match", { userId, socketId: socket.id, difficulty })
-        startCountDown();
+    socket.emit("find-match", { userId, socketId: socket.id, difficulty });
+    startCountDown();
 
-        // Server notifies client that a match is found
-        socket.on("found-match", () => {
-            console.log("found match!");
+    // Server notifies client that a match is found
+    socket.on("found-match", () => {
+      console.log("found match!");
 
-            socket.on("room", (roomId) => {
-                console.log(roomId);
-                clearTimeout(startCountDown());
-                socket.disconnect();
-                closeModal();
-
-                //TODO: redirect to room page
-            })
-        })
-    });
-
-    const cleanUp = () => {
+      socket.on("room", (roomId) => {
+        console.log(roomId);
         clearTimeout(startCountDown());
         socket.disconnect();
         closeModal();
-    }
 
-    return (
-        <Dialog
-            open={open}
+        navigate(`/room`);
+        // navigate(`/room/${roomId}`);
+      });
+    });
+  });
+
+  const cleanUp = () => {
+    clearTimeout(startCountDown());
+    socket.disconnect();
+    closeModal();
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle>Finding match...</DialogTitle>
+
+      <Box sx={{ width: 400, height: 180 }}>
+        <Stack
+          sx={{ height: "100%" }}
+          justifyContent="space-around"
+          alignItems="center"
         >
-            <DialogTitle>
-                Finding match...
-            </DialogTitle>
+          <CircularProgress size={60} />
 
-            <Box sx={{ width: 400, height: 180 }}>
-                <Stack sx={{ height: "100%" }} justifyContent="space-around" alignItems="center">
-                    <CircularProgress size={60} />
-
-                    <Button variant="contained" onClick={cleanUp}>
-                        Cancel
-                    </Button>
-                </Stack>
-            </Box>
-        </Dialog>
-    );
-}
+          <Button variant="contained" onClick={cleanUp}>
+            Cancel
+          </Button>
+        </Stack>
+      </Box>
+    </Dialog>
+  );
+};
 
 export default LoadingModal;
