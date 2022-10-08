bf2e73c2b2c390598a583bc022db3a4200d87ebc
jzwoo <jzwoo98@gmail.com>
2022-10-09 00:55:51 +0800

    Implement confirmation dialog

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,16 +8,37 @@ import {
   MenuItem,
 } from '@mui/material';
 import { SchoolSharp, SettingsSharp } from '@mui/icons-material';
+import ConfirmationDialog from './modal/ConfirmationDialog';
+import { useAuth } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
+  const authClient = useAuth();
+  const navigate = useNavigate();
 
-  const handleClose = () => {
+  const handleOpenSettingsMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseSettingsMenu = () => {
     setAnchorEl(null);
   };
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+  const handleDeleteAccount = () => {
+    setAnchorEl(null);
+    setConfirmDialogOpen(true);
+  };
+
+  const handleDeleteUser = () => {
+    try {
+      authClient.deleteUser();
+      navigate('/');
+    } catch (err) {
+      // TODO: error handling
+      console.log(err);
+    }
   };
 
   return (
@@ -37,38 +58,40 @@ function Navbar() {
           PeerPrep
         </Typography>
 
-        <div>
-          <IconButton
-            aria-controls='menu-appbar'
-            aria-haspopup='true'
-            onClick={handleMenu}
-            color='inherit'
-          >
-            <SettingsSharp />
-          </IconButton>
+        <IconButton onClick={handleOpenSettingsMenu} color='inherit'>
+          <SettingsSharp />
+        </IconButton>
 
-          <Menu
-            id='menu-appbar'
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Change username</MenuItem>
-            <MenuItem onClick={handleClose}>Change password</MenuItem>
-            <MenuItem onClick={handleClose}>Delete account</MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-          </Menu>
-        </div>
+        <Menu
+          id='menu-appbar'
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleCloseSettingsMenu}
+        >
+          <MenuItem onClick={handleCloseSettingsMenu}>Change username</MenuItem>
+          <MenuItem onClick={handleCloseSettingsMenu}>Change password</MenuItem>
+          <MenuItem onClick={handleDeleteAccount} sx={{ color: 'red' }}>
+            Delete account
+          </MenuItem>
+          <MenuItem onClick={handleCloseSettingsMenu}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
+
+      <ConfirmationDialog
+        dialogOpen={confirmDialogOpen}
+        setDialogOpen={setConfirmDialogOpen}
+        message={'Confirm the deletion of your account?'}
+        onConfirmAction={handleDeleteUser}
+      ></ConfirmationDialog>
     </AppBar>
   );
 }
