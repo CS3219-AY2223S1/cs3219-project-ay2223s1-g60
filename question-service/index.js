874ff944cbe01128d8a3816298bf55b885a9f300
Florencia Martina <florenciamartina15@gmail.com>
2022-10-14 23:07:08 +0800

    Change port number for question service

--- question-service/index.js
+++ question-service/index.js
@@ -18,4 +18,4 @@ app.use("/api/question", router).all((_, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 });
 
-app.listen(8000, () => console.log("question-service listening on port 8000"));
+app.listen(8004, () => console.log("question-service listening on port 8004"));
