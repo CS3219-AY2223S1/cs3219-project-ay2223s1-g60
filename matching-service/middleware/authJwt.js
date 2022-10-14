2949b4260582f2584764702663f27145ad71fe69
Florencia Martina <florenciamartina15@gmail.com>
2022-10-14 13:56:51 +0800

    Refactor verifyRoomUser

--- matching-service/middleware/authJwt.js
+++ matching-service/middleware/authJwt.js
@@ -1,42 +1,27 @@
-import jwt from "jsonwebtoken";
+import { decodeToken, isValidRequest } from "../../utils/token";
+
+const isVerifiedUser = (decoded, username, roomId) => {
+  return (
+    (decoded.username1 == username || decoded.username2 == username) &&
+    decoded.roomId == roomId
+  );
+};
 
 export async function verifyRoomUser(req, res) {
-  console.log("Auth: " + JSON.stringify(req.headers));
-  console.log("Body: " + JSON.stringify(req.body));
-  if (
-    req.body &&
-    req.headers &&
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "Bearer"
-  ) {
-    const { username, roomId } = req.body;
-    const tokenUser = req.headers.authorization.split(" ")[1];
-    try {
-      jwt.verify(
-        tokenUser,
-        process.env.JWT_ROOM_PRIVATE_KEY,
-        function (err, decoded) {
-          console.log("Decoded: " + JSON.stringify(decoded));
-          console.log("Error : " + JSON.stringify(err));
-          if (err) {
-            throw err;
-          }
-          if (
-            (decoded.username1 == username || decoded.username2 == username) &&
-            decoded.roomId == roomId
-          ) {
-            return res
-              .status(201)
-              .json({ message: "User is verified. Joining the room." });
-          } else {
-            return res.status(401).json({ message: "Unauthorized access" });
-          }
-        }
-      );
-    } catch (error) {
-      return res.status(401).json({ message: "Invalid JWT token!" });
-    }
-  } else {
+  if (!isValidRequest(req)) {
     return res.status(401).json({ message: "Missing JWT token!" });
+  }
+  const { username, roomId } = req.body;
+  const tokenUser = req.headers.authorization.split(" ")[1];
+  try {
+    const decoded = decodeToken(tokenUser, process.env.JWT_ROOM_PRIVATE_KEY);
+    if (!isVerifiedUser(decoded, username, roomId)) {
+      return res.status(401).json({ message: "Unauthorized access" });
+    }
+    return res
+      .status(201)
+      .json({ message: "User is verified. Joining the room." });
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid JWT token!" });
   }
 }
---
+++ utils/token.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const isValidRequest = (req) => {
+  return (
+    req.body &&
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  );
+};
+
+export const decodeToken = (token, privateKey) => {
+  const verifiedToken = jwt.verify(token, privateKey, function (err, decoded) {
+    console.log("Decoded: " + JSON.stringify(decoded));
+    console.log("Error : " + JSON.stringify(err));
+    if (err) {
+      throw err;
+    }
+
+    return decoded;
+  });
+  return verifiedToken;
+};
