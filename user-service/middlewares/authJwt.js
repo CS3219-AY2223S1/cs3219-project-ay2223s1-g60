68622650d1bfa01df5adeae5d7f5d9f05108bcc3
Florencia Martina <florenciamartina15@gmail.com>
2022-10-14 13:47:39 +0800

    Extract conditions into functions in verify token

--- user-service/middlewares/authJwt.js
+++ user-service/middlewares/authJwt.js
@@ -1,51 +1,54 @@
 import { ormGetToken as _getToken } from "../model/user-orm.js";
 import jwt from "jsonwebtoken";
 
-export async function verifyToken(req, res, next) {
-  if (
+const isValidRequest = (req) => {
+  return (
     req.body.username &&
     req.headers &&
     req.headers.authorization &&
     req.headers.authorization.split(" ")[0] === "Bearer"
-  ) {
-    const username = req.body.username;
-    const tokenFromDb = await _getToken(username);
-    const tokenFromUser = req.headers.authorization.split(" ")[1];
-    try {
-      const fromDb = jwt.verify(
-        tokenFromDb,
-        process.env.JWT_PRIVATE_KEY,
-        function (err, decodedFromDb) {
-          if (err) {
-            throw err;
-          }
-          return decodedFromDb;
-        }
-      );
-      const fromUser = jwt.verify(
-        tokenFromUser,
-        process.env.JWT_PRIVATE_KEY,
-        function (err, decodedFromUser) {
-          if (err) {
-            throw err;
-          }
-          return decodedFromUser;
-        }
-      );
+  );
+};
 
-      if (
-        fromDb.username === fromUser.username &&
-        fromDb.hashedPassword === fromUser.hashedPassword &&
-        fromDb._id === fromUser._id
-      ) {
-        next();
-      } else {
-        return res.status(401).json({ message: "Unauthorized access" });
+const isMatchingCredential = (fromDb, fromUser) => {
+  return (
+    fromDb.username === fromUser.username &&
+    fromDb.hashedPassword === fromUser.hashedPassword &&
+    fromDb._id === fromUser._id
+  );
+};
+
+const decodeToken = (token) => {
+  const decodedToken = jwt.verify(
+    token,
+    process.env.JWT_PRIVATE_KEY,
+    function (err, decoded) {
+      if (err) {
+        throw err;
       }
-    } catch (err) {
-      return res.status(401).json({ message: "Invalid JWT token!" });
+      return decoded;
     }
-  } else {
+  );
+  return decodedToken;
+};
+
+export async function verifyToken(req, res, next) {
+  if (!isValidRequest(req)) {
     return res.status(401).json({ message: "Missing JWT token!" });
+  }
+
+  const username = req.body.username;
+  const tokenFromDb = await _getToken(username);
+  const tokenFromUser = req.headers.authorization.split(" ")[1];
+  try {
+    const fromDb = decodeToken(tokenFromDb);
+    const fromUser = decodeToken(tokenFromUser);
+
+    if (!isMatchingCredential(fromDb, fromUser)) {
+      return res.status(401).json({ message: "Unauthorized access" });
+    }
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid JWT token!" });
   }
 }
