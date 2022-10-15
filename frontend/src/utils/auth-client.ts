6a3818c7012a309556e9b669a835d6e735493efe
florenciamartina <65345305+florenciamartina@users.noreply.github.com>
2022-10-15 17:05:09 +0800

    Implement Room Authorization (#72)
    * Remove room-controller
    
    * Implement room authorization
    
    * Refactor verifyRoomUser
    
    * Create function to save and get room token
    
    * Add snackbar on room page access
    
    * Remove success snackbar

--- frontend/src/utils/auth-client.ts
+++ frontend/src/utils/auth-client.ts
@@ -9,71 +9,21 @@ import {
   USER_LOGOUT,
   USER_SIGNUP,
 } from '../configs';
-
-declare namespace API {
-  type Response<T> = {
-    status: number;
-    statusText: string;
-    data: T;
-  };
-}
-
-const instance = axios.create({
-  baseURL: URL_USER_SVC,
-  timeout: 15000,
-});
-
-const responseBody = (response: AxiosResponse) => {
-  const res: API.Response<typeof response.data> = {
-    status: response.status,
-    statusText: response.statusText,
-    data: response.data,
-  };
-
-  return res;
-};
-
-const requests = {
-  get: (url: string) =>
-    instance
-      .get(url)
-      .then(responseBody)
-      .catch((err) => responseBody(err.response)),
-  post: (url: string, body: {}) =>
-    instance
-      .post(url, body)
-      .then(responseBody)
-      .catch((err) => responseBody(err.response)),
-  postWithHeaders: (url: string, body: {}, headers: {}) =>
-    instance
-      .post(url, body, headers)
-      .then(responseBody)
-      .catch((err) => responseBody(err.response)),
-  put: (url: string, body: {}) =>
-    instance
-      .put(url, body)
-      .then(responseBody)
-      .catch((err) => responseBody(err.response)),
-  delete: (url: string, body: {}) =>
-    instance
-      .delete(url, body)
-      .then(responseBody)
-      .catch((err) => responseBody(err.response)),
-};
+import { requests, API } from './api-request';
 
 export const AuthClient = {
   signUp: (body: {
     username: string;
     password: string;
   }): Promise<API.Response<{ message: string }>> =>
-    requests.post(USER_SIGNUP, body),
+    requests.post(URL_USER_SVC, USER_SIGNUP, body),
 
   loginWithUname: (body: {
     username: string;
     password: string;
   }): Promise<
     API.Response<{ username: string; token: string; message: string }>
-  > => requests.post(USER_LOGIN, body),
+  > => requests.post(URL_USER_SVC, USER_LOGIN, body),
 
   loginWithToken: (
     token: string,
@@ -85,6 +35,7 @@ export const AuthClient = {
     };
 
     return requests.postWithHeaders(
+      URL_USER_SVC,
       USER_LOGIN_WITH_TOKEN,
       { username },
       { headers }
@@ -95,7 +46,7 @@ export const AuthClient = {
     token: string;
     username: string;
   }): Promise<API.Response<{ message: string }>> => {
-    return requests.post(USER_LOGOUT, body);
+    return requests.post(URL_USER_SVC, USER_LOGOUT, body);
   },
 
   changeUsername: (body: {
@@ -103,7 +54,7 @@ export const AuthClient = {
     newUsername: string;
     password: string;
   }): Promise<API.Response<{ message: string }>> => {
-    return requests.post(USER_CHANGE_USERNAME, body);
+    return requests.post(URL_USER_SVC, USER_CHANGE_USERNAME, body);
   },
 
   changePassword: (body: {
@@ -111,12 +62,12 @@ export const AuthClient = {
     oldPassword: string;
     newPassword: string;
   }): Promise<API.Response<{ message: string }>> => {
-    return requests.post(USER_CHANGE_PASSWORD, body);
+    return requests.post(URL_USER_SVC, USER_CHANGE_PASSWORD, body);
   },
 
   deleteUser: (data: {
     username: string;
   }): Promise<API.Response<{ message: string }>> => {
-    return requests.delete(USER_DELETE_USER, { data });
+    return requests.delete(URL_USER_SVC, USER_DELETE_USER, { data });
   },
 };
--- matching-service/controller/socket-controller.js
+++ matching-service/controller/socket-controller.js
@@ -1,12 +1,13 @@
 import {
   ormCreateRoom as createRoom,
   ormDeleteRoom as deleteRoom,
-} from '../model/room-orm.js';
-import axios from 'axios';
+} from "../model/room-orm.js";
+import jwt from "jsonwebtoken";
+import axios from "axios";
 
 const waitingRoom = [];
-const ROOM_URL = 'http://localhost:8001/api/room';
-const QUESTION_URL = 'http://localhost:8004/api/question';
+const ROOM_URL = "http://localhost:8001/api/room";
+const QUESTION_URL = "http://localhost:8004/api/question";
 
 // Finds match from waiting room, returns -1 if unavailable
 const findMatch = (req) => {
@@ -41,15 +42,27 @@ const onFindMatchEvent = (req, io) => {
   if (index < 0) {
     addWaitingUser(req);
   } else {
-    io.to(waitingRoom[index].socketId).emit('found-match');
-    io.to(req.socketId).emit('found-match');
+    io.to(waitingRoom[index].socketId).emit("found-match");
+    io.to(req.socketId).emit("found-match");
 
     // create room using orm
     createRoom(waitingRoom[index].username, req.username, req.difficulty).then(
       (res) => {
         if (!res.err) {
-          io.to(waitingRoom[index].socketId).emit('join-room', res.roomId);
-          io.to(req.socketId).emit('join-room', res.roomId);
+          const roomToken = generateRoomToken(
+            waitingRoom[index].username,
+            req.username,
+            res.roomId
+          );
+          io.to(waitingRoom[index].socketId).emit("join-room", {
+            roomId: res.roomId,
+            token: roomToken,
+          });
+          io.to(req.socketId).emit("join-room", {
+            roomId: res.roomId,
+            token: roomToken,
+          });
+
           removeWaitingUser(waitingRoom[index].socketId);
           onGetQuestionEvent(io, { room: res.roomId });
           return res;
@@ -61,6 +74,21 @@ const onFindMatchEvent = (req, io) => {
   }
 };
 
+const generateRoomToken = (username1, username2, roomId) => {
+  let privateRoomKey = process.env.JWT_ROOM_PRIVATE_KEY;
+  let token = jwt.sign(
+    {
+      username1: username1,
+      username2: username2,
+      roomId: roomId,
+    },
+    privateRoomKey,
+    { expiresIn: "2h" }
+  );
+  console.log("Room token: " + token);
+  return token;
+};
+
 const onDisconnectEvent = (socket) => {
   removeWaitingUser(socket.id);
   console.log(`Disconnected with ${socket.id}`);
@@ -68,7 +96,7 @@ const onDisconnectEvent = (socket) => {
 
 const onDeleteRoomEvent = (req) => {
   deleteRoom(req.room).then((res) =>
-    res.err ? console.log(res.message) : console.log('Delete room: ', req.room)
+    res.err ? console.log(res.message) : console.log("Delete room: ", req.room)
   );
 };
 
@@ -80,23 +108,23 @@ const onGetQuestionEvent = async (io, { room }) => {
     const questionObj = await axios.get(
       `${QUESTION_URL}?difficulty=${difficulty}`
     );
-    io.to(room).emit('question', questionObj.data.resp);
+    io.to(room).emit("question", questionObj.data.resp);
     const updatedRoom = await axios.put(`${ROOM_URL}`, {
       roomId: room,
       question: questionObj.data.resp,
     });
     return updatedRoom;
   } catch (err) {
-    console.log('ERROR GET QUESTION: ' + err);
+    console.log("ERROR GET QUESTION: " + err);
   }
 };
 
 const createEventListeners = (socket, io) => {
-  socket.on('find-match', (req) => onFindMatchEvent(req, io));
-  socket.on('disconnect', () => onDisconnectEvent(socket));
-  socket.on('delete-room', (req) => onDeleteRoomEvent(req));
-  socket.on('get-question', async (room) => onGetQuestionEvent(io, room));
-  socket.on('join-room', ({ room }) => socket.join(room));
+  socket.on("find-match", (req) => onFindMatchEvent(req, io));
+  socket.on("disconnect", () => onDisconnectEvent(socket));
+  socket.on("delete-room", (req) => onDeleteRoomEvent(req));
+  socket.on("get-question", async (room) => onGetQuestionEvent(io, room));
+  socket.on("join-room", ({ room }) => socket.join(room));
 };
 
 export default createEventListeners;
