e3c77d9e69937c32e59065c716a293413ee800da
Christian Drake Martin <60537545+drake25122000@users.noreply.github.com>
2022-11-02 16:57:54 +0800

    Fix error code and message (#97)
    * Fix error message
    
    * Change error message
    
    * Add error message when username and/or password are wrong

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -22,21 +22,18 @@ export async function createUser(req, res) {
     if (username && password) {
       const hashedPassword = await bcrypt.hash(password, saltRounds);
       const resp = await _createUser(username, hashedPassword);
-      console.log("response controller: ");
-      console.log(resp);
       if (resp.err) {
         if (
           resp.err.name &&
           resp.err.name === "MongoServerError" &&
           resp.err.code === 11000
         ) {
-          return res.status(409).json({ message: "Duplicate user!" });
+          return res.status(409).json({ message: `User ${username} already exists!` });
         }
         return res
           .status(400)
           .json({ message: "Could not create a new user!" });
       } else {
-        console.log(`Created new user ${username} successfully!`);
         return res
           .status(201)
           .json({ message: `Created new user ${username} successfully!` });
@@ -47,7 +44,6 @@ export async function createUser(req, res) {
         .json({ message: "Username and/or Password are missing!" });
     }
   } catch (err) {
-    console.log("Here error ", err);
     return res
       .status(500)
       .json({ message: "Database failure when creating new user!" });
@@ -59,11 +55,14 @@ export async function signIn(req, res) {
     const { username, password } = req.body;
     if (username && password) {
       const user = await _getUser(username, password);
+
+      if (!user) {
+        return res.status(400).json({ message: "Wrong username and/or password!" });
+      }
+
       if (user.err) {
         return res.status(400).json({ message: "Could not sign in!" });
       } else {
-        console.log(`Signed in user ${username} successfully!`);
-
         let token = await generateToken(user);
 
         const updated = await _addToken(username, token);
@@ -79,7 +78,7 @@ export async function signIn(req, res) {
         .json({ message: "Username and/or Password are missing!" });
     }
   } catch (err) {
-    return res.status(500).json({ message: "Could not found user" });
+    return res.status(500).json({ message: "Database failure when signing in!" });
   }
 }
 
@@ -109,7 +108,7 @@ export async function changePassword(req, res) {
         .json({ message: "Username and/or Passwords are missing!" });
     }
   } catch (err) {
-    return res.status(500).json({ message: "Could not found user" });
+    return res.status(500).json({ message: "Database failure when changing password!" });
   }
 }
 
@@ -136,7 +135,7 @@ export async function changeUsername(req, res) {
         .json({ message: "Username and/or Password are missing!" });
     }
   } catch (err) {
-    return res.status(500).json({ message: "Could not found user" });
+    return res.status(500).json({ message: "Database failure when changing username!" });
   }
 }
 
@@ -147,7 +146,6 @@ export async function deleteUser(req, res) {
     const { username } = req.body;
     if (username) {
       const isDeleted = await _deleteUser(username);
-      console.log("Controller: " + JSON.stringify(isDeleted));
       if (!isDeleted) {
         return res.status(400).json({ message: "User does not exist!" });
       } else if (isDeleted) {
@@ -157,7 +155,7 @@ export async function deleteUser(req, res) {
       return res.status(400).json({ message: "Username is missing!" });
     }
   } catch (err) {
-    return res.status(500).json({ message: "Could not found user" });
+    return res.status(500).json({ message: "Database failure when changing username!" });
   }
 }
 
@@ -173,7 +171,6 @@ export async function generateToken(user) {
     privateKey,
     { expiresIn: "2h" }
   );
-  console.log(token);
   return token;
 }
 
@@ -224,8 +221,7 @@ export async function logout(req, res) {
         .json({ message: "Username and/or token are missing!" });
     }
   } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "Error in logging out" });
+    return res.status(500).json({ message: "Database failure when logging out!" });
   }
 }
 
