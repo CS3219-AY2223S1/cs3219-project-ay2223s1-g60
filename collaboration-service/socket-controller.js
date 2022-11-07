0486b3aeef40c18fb391c44171288a1c3a9159f8
jzwoo <jzwoo98@gmail.com>
2022-11-07 23:03:32 +0800

    Fix room refresh problem for code

--- collaboration-service/socket-controller.js
+++ collaboration-service/socket-controller.js
@@ -1,12 +1,21 @@
+// stores the current session's code
+let savedData;
+
 const createEventListeners = (socket, io) => {
   socket.on('join-room', (res) => {
     console.log('Joined room: ', res.room);
     socket.join(res.room);
     io.to(socket.id).emit('joined-room', { room: res.room });
+
+    if (savedData) {
+      io.to(res.room).emit('typedCode', savedData);
+    }
   });
 
   socket.on('typedCode', (data) => {
     io.to(data.room).emit('typedCode', data);
+    // save the data into a global var
+    savedData = data;
   });
 
   socket.on('set-language', (data) => {
